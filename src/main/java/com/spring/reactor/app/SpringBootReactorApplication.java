49b75025cbf4026d729986e9d405dedd6a5da22d
joseusuga <jose.usuga@ceiba.com.co>
2020-11-23 14:33:50 -0500

    Agregar modelo usuario para manipular objetos con usuos del map, mostrarlos por logs

--- src/main/java/com/spring/reactor/app/SpringBootReactorApplication.java
+++ src/main/java/com/spring/reactor/app/SpringBootReactorApplication.java
@@ -1,5 +1,6 @@
 package com.spring.reactor.app;
 
+import com.spring.reactor.app.models.Usuario;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -21,15 +22,20 @@ public class SpringBootReactorApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        Flux<String> nombres = Flux.just("Jose", "Luis", "Carlos", "Juana")
-                .doOnNext(e -> {
-                    if (e.isEmpty())
+        Flux<Usuario> nombres = Flux.just("Jose", "Luis", "Carlos", "Juana")
+                .map(nombre -> new Usuario(nombre.toUpperCase(), null))
+                .doOnNext(usuario -> {
+                    if (usuario == null)
                         throw new RuntimeException("Los nombres no pueden estar vacios");
 
-                    System.out.println(e);
+                    System.out.println(usuario.getNombre());
+                }).map(usuario -> {
+                    String nombre = usuario.getNombre().toLowerCase();
+                    usuario.setNombre(nombre);
+                    return usuario;
                 });
 
-        nombres.subscribe(e -> LOGGER.info(e), error -> LOGGER.error(error.getMessage()), new Runnable() {
+        nombres.subscribe(e -> LOGGER.info(e.toString()), error -> LOGGER.error(error.getMessage()), new Runnable() {
             @Override
             public void run() {
                 LOGGER.info("Ha finalizado con éxito la ejecución");
