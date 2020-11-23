68a311b4aec0375c20764817dc161a5d58f770c9
joseusuga <jose.usuga@ceiba.com.co>
2020-11-23 14:18:25 -0500

    Terminar de validar y ejecutar al finalizar la ejecución del suscribe en el flux, onComplete

--- src/main/java/com/spring/reactor/app/SpringBootReactorApplication.java
+++ src/main/java/com/spring/reactor/app/SpringBootReactorApplication.java
@@ -1,5 +1,7 @@
 package com.spring.reactor.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +12,8 @@ import reactor.core.publisher.Flux;
  */
 @SpringBootApplication
 public class SpringBootReactorApplication implements CommandLineRunner {
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(SpringBootReactorApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootReactorApplication.class, args);
@@ -17,9 +21,19 @@ public class SpringBootReactorApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        Flux<String> nombres = Flux.just("Jose", "Luis", "Juana")
-                .doOnNext(System.out::println);
+        Flux<String> nombres = Flux.just("Jose", "Luis", "Carlos", "Juana")
+                .doOnNext(e -> {
+                    if (e.isEmpty())
+                        throw new RuntimeException("Los nombres no pueden estar vacios");
 
-        nombres.subscribe();
+                    System.out.println(e);
+                });
+
+        nombres.subscribe(e -> LOGGER.info(e), error -> LOGGER.error(error.getMessage()), new Runnable() {
+            @Override
+            public void run() {
+                LOGGER.info("Ha finalizado con éxito la ejecución");
+            }
+        });
     }
 }
