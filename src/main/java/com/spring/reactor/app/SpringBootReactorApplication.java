bc7b764d9adb826af2e9e70e808066dcc56ff907
joseusuga <jose.usuga@ceiba.com.co>
2020-11-23 13:53:40 -0500

    GRAL: Base de proyecto reactivo, que corre sólo en consola

---
+++ src/main/java/com/spring/reactor/app/SpringBootReactorApplication.java
@@ -0,0 +1,22 @@
+package com.spring.reactor.app;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Flux;
+
+/**
+ * CommandLineRunner para que nuestra app sea por liena de comandos
+ */
+@SpringBootApplication
+public class SpringBootReactorApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootReactorApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
